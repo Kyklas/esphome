2792124bfd33e7ddf6e3819e0ccda2b7b5958c03
Stan Bertrand <stanislasbertrand@gmail.com>
2024-01-07 19:20:26 -0800

    components: max6921 iv18: sample
    Displays time and provide a countdown timer.

---
+++ esphome/components/max6921iv18/display.py
@@ -0,0 +1,50 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.components import display
+from esphome.const import CONF_ID, CONF_INTENSITY, CONF_DATA_PIN, CONF_CLOCK_PIN, CONF_ENABLE_PIN, CONF_LAMBDA
+
+DEPENDENCIES = []
+
+max6921iv18_ns = cg.esphome_ns.namespace('max6921iv18')
+MAX6921IV18Component = max6921iv18_ns.class_('MAX6921IV18Component', cg.Component)
+MAX6921IV18ComponentRef = MAX6921IV18Component.operator('ref')
+
+CONF_MAX6921IV18 = 'max6921iv18'
+CONF_LATCH_PIN = 'latch_pin'
+CONF_OE_PIN = 'oe_pin'
+
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(MAX6921IV18Component),
+    cv.Required(CONF_ENABLE_PIN): pins.gpio_output_pin_schema,
+    cv.Required(CONF_DATA_PIN): pins.internal_gpio_input_pin_schema,
+    cv.Required(CONF_CLOCK_PIN): pins.internal_gpio_input_pin_schema,
+    cv.Required(CONF_LATCH_PIN): pins.gpio_output_pin_schema,
+    cv.Required(CONF_OE_PIN): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_INTENSITY, default=15): cv.int_range(min=0, max=15),
+}).extend(cv.polling_component_schema('1s'))
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield display.register_display(var, config)
+
+    enable_pin = yield cg.gpio_pin_expression(config[CONF_ENABLE_PIN])
+    cg.add(var.set_enable_pin(enable_pin))
+    data_pin = yield cg.gpio_pin_expression(config[CONF_DATA_PIN])
+    cg.add(var.set_data_pin(data_pin))
+    clock_pin = yield cg.gpio_pin_expression(config[CONF_CLOCK_PIN])
+    cg.add(var.set_clock_pin(clock_pin))
+    latch_pin = yield cg.gpio_pin_expression(config[CONF_LATCH_PIN])
+    cg.add(var.set_latch_pin(latch_pin))
+    oe_pin = yield cg.gpio_pin_expression(config[CONF_OE_PIN])
+    cg.add(var.set_oe_pin(oe_pin))
+
+    cg.add(var.set_intensity(config[CONF_INTENSITY]))
+
+    if CONF_LAMBDA in config:
+        lambda_ = yield cg.process_lambda(config[CONF_LAMBDA], [(MAX6921IV18ComponentRef, 'it')],
+                                          return_type=cg.void)
+        cg.add(var.set_writer(lambda_))
+
---
+++ esphome/components/max6921iv18/max6921iv18.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+
+#include "esphome/core/defines.h"
+
+#ifdef USE_TIME
+#include "esphome/components/time/real_time_clock.h"
+#endif
+
+#include "driver/spi_master.h"
+
+namespace esphome
+{
+namespace max6921iv18
+{
+
+class MAX6921IV18Component;
+
+using max6921iv18_writer_t = std::function<void(MAX6921IV18Component &)>;
+
+// Help track spi xfer
+typedef struct _MAX6921IV18_Digit
+{
+	spi_transaction_t xfer;
+	MAX6921IV18Component *max6921iv18comp;
+	uint8_t idx;
+} MAX6921IV18_Digit;
+
+class MAX6921IV18Component : public PollingComponent
+{
+  public:
+	MAX6921IV18Component() = default;
+
+	void setup() override;
+	float get_setup_priority() const override;
+	void dump_config() override;
+
+	void set_enable_pin(GPIOPin *pin)
+	{
+		ESP_LOGD("IV18-H", __func__);
+		enable_pin_ = pin;
+	}
+	void set_data_pin(InternalGPIOPin *pin)
+	{
+		ESP_LOGD("IV18-H", __func__);
+		data_pin_ = pin;
+	}
+	void set_clock_pin(InternalGPIOPin *pin)
+	{
+		ESP_LOGD("IV18-H", __func__);
+		clock_pin_ = pin;
+	}
+	void set_latch_pin(GPIOPin *pin)
+	{
+		ESP_LOGD("IV18-H", __func__);
+		latch_pin_ = pin;
+	}
+	void set_oe_pin(GPIOPin *pin)
+	{
+		ESP_LOGD("IV18-H", __func__);
+		oe_pin_ = pin;
+	}
+
+	void display();
+	void set_intensity(uint8_t intensity);
+
+	void update() override;
+
+	void set_writer(max6921iv18_writer_t &&writer);
+
+	/// Evaluate the printf-format and print the result at the given position.
+	uint8_t printf(uint8_t pos, const char *format, ...)
+		__attribute__((format(printf, 3, 4)));
+	/// Evaluate the printf-format and print the result at position 0.
+	uint8_t printf(const char *format, ...)
+		__attribute__((format(printf, 2, 3)));
+
+	/// Print `str` at the given position.
+	uint8_t print(uint8_t pos, const char *str);
+	/// Print `str` at position 0.
+	uint8_t print(const char *str);
+
+#ifdef USE_TIME
+	/// Evaluate the strftime-format and print the result at the given position.
+	uint8_t strftime(uint8_t pos, const char *format, ESPTime time)
+		__attribute__((format(strftime, 3, 0)));
+
+	/// Evaluate the strftime-format and print the result at position 0.
+	uint8_t strftime(const char *format, ESPTime time)
+		__attribute__((format(strftime, 2, 0)));
+#endif
+
+  protected:
+	GPIOPin *enable_pin_;
+	InternalGPIOPin *data_pin_;
+	InternalGPIOPin *clock_pin_;
+	GPIOPin *latch_pin_;
+	GPIOPin *oe_pin_;
+
+	uint8_t intensity_{15}; /// Intensity of the display from 0 to 15 (most)
+
+	optional<max6921iv18_writer_t> writer_{};
+
+	spi_device_handle_t spi;
+
+	uint8_t active;
+	MAX6921IV18_Digit digits[2][9];
+	uint8_t digits_active;
+
+	void set_digit(uint8_t idx, uint8_t val, uint8_t seg);
+
+	static void spi_post_xfer(spi_transaction_t *trans);
+};
+
+} // namespace max6921iv18
+} // namespace esphome
